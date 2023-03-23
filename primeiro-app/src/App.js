5ad52f85a6060da99af7a7e85a34917bee323237
denervalspelizario <denervalspelizario@gmail.com>
2023-03-23 18:45:17 -0300

    estudo nextjs revisao de manipulacao de dados de input atarvez de states

--- primeiro-app/src/App.js
+++ primeiro-app/src/App.js
@@ -1,31 +1,80 @@
 
-import Nome from "./components/Nome"
 import { useState } from "react"
 
 
-export default function App({props}){
+export default function App(){
 
-const [profissao, setProfissao] = useState('Desenvolvedor Web')
+const [nome, setNome] = useState('')
+const [email, setEmail] = useState('')
+const [idade, setIdade] = useState('')
 
-function alteraProfissao(nome){
-  setProfissao(nome)
-  alert('alterou a profissao')
+
+const [user, setUser] = useState({}) // como é um objeto que recebe varios dados no caso os dados do input deve conter o {}
+
+
+function cadastrar(e){
+  e.preventDefault() // o preventdefault é usado para evitar que ao clicar no btn a pagina atualize automaticamente 
+  
+  setUser({          // adicionando ao state user as states que sao os dados dos inputs
+      nome: nome,
+      email: email,
+      idade:  idade
+    })  
+
+  
 }
+
+
+
+
 
 
   return(
     <div>
-        <h1>Componente App</h1>
-        <button 
-          onClick={() => alteraProfissao('Desenvolvedor Mobile')}
-          >
-          clique aqui
-        </button>
-        <h2>{profissao}</h2>
-        <br/>
-        <Nome nome='Denerval' idade={30}/>
-        <br/>
-        <Nome nome='Tom' idade={36}/>
+
+      <h1>Cadasro de usuario</h1>
+        <form onSubmit={cadastrar}>
+          <label>
+            Digite seu nome
+          </label><br/>
+            <input  placeholder="Digite seu nome"
+                    value={nome}
+                    onChange={(e) => setNome(e.target.value)}
+            
+            /><br/>
+
+            <label>
+            Digite seu email
+          </label><br/>
+            <input  placeholder="Digite seu email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+            
+            /><br/>
+
+            <label>
+            Digite seu idade
+          </label><br/>
+            <input  placeholder="Digite seu idade"
+                    value={idade}
+                    onChange={(e) => setIdade(e.target.value)}
+            
+            />  <br/>
+
+            <button type="submit">
+              Registrar
+            </button>
+        </form>
+
+        <br/><br/>
+        <div>
+          <span>Bem vindo: {user.nome}</span><br/>
+          <span>idade: {user.idade}</span><br/>
+          <span>email: {user.email}</span><br/>
+
+        </div>
+
+
     </div>
   )
 }
